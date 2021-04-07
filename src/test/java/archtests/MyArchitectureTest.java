51b5a2fa748fca7bfdbfc357f1378fd84e9167ce
Alexandru Bolboaca <alexboly@gmail.com>
2021-04-07 16:19:31 +0300

    First passing architectural test, and another one that doesn't pass

--- src/test/java/archtests/MyArchitectureTest.java
+++ src/test/java/archtests/MyArchitectureTest.java
@@ -1,3 +1,5 @@
+package archtests;
+
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.ArchRule;
@@ -6,12 +8,21 @@ import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import org.junit.jupiter.api.Test;
 
 public class MyArchitectureTest {
-    @Test
-    public void daoClassNamesShouldEndInDao() {
-        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.book.dao");
-    
-        ArchRule rule = classes().should().haveSimpleNameEndingWith("Dao");
-    
-        rule.check(importedClasses);
-    }
+	@Test
+	public void daoClassNamesShouldEndInDao() {
+		JavaClasses importedClasses = new ClassFileImporter().importPackages("com.book.dao");
+
+		ArchRule rule = classes().should().haveNameMatching(".*Dao(\\$.)*");
+
+		rule.check(importedClasses);
+	}
+
+	@Test
+	public void daoClassesShouldHaveOnlyFinalFields() {
+		JavaClasses importedClasses = new ClassFileImporter().importPackages("com.book.dao");
+
+		ArchRule rule = classes().should().haveOnlyFinalFields();
+
+		rule.check(importedClasses);
+	}
 }
